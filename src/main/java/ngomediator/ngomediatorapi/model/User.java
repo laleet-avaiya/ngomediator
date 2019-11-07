02689cd759ee1443b6555bc3efaf5a5a64e3ecfc
lmavaiya <42064785+lmavaiya@users.noreply.github.com>
2019-11-07 10:45:17 +0530

    API Updated

--- src/main/java/ngomediator/ngomediatorapi/model/User.java
+++ src/main/java/ngomediator/ngomediatorapi/model/User.java
@@ -9,6 +9,8 @@ import java.util.Date;
 public class User {
 
 
+
+
     @Id
     private String id;
     private String name;
@@ -27,6 +29,10 @@ public class User {
 
     public String getId() {
         return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getName() {
